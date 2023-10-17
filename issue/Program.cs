05025765db86771a525d51aca8026fc0839dc8c4
Roman Fomin <fomin.roman.andr@gmail.com>
2023-10-17 20:36:00 +0300

    feat: add breaking changes

--- issue/Program.cs
+++ issue/Program.cs
@@ -1,9 +1,30 @@
-﻿public class Program
+﻿using Microsoft.Data.Sqlite;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         var item = new Item() { Id = 1, Data = 1 };
-
+        DoSomeStuffWithDB();
         Console.WriteLine($"Hello World!\n{item.Id}) - {item.Data}");
+    }
+
+    public static void DoSomeStuffWithDB()
+    {
+        using var conn = new SqliteConnection($"Data Source=items2.db; Foreign Keys=true; Mode=ReadOnly");
+        conn.Open();
+        string commandText = @"SELECT * FROM Items;";
+
+        var command = new SqliteCommand(commandText, conn);
+        using var reader = command.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var data = reader.GetInt32(1);
+                Console.WriteLine($"{id}) - {data}");
+            }
+        }
     }
 }
--- model/Program.cs
+++ model/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Item
+﻿using Microsoft.Data.Sqlite;
+
+public class Item
 {
     public int Id { get; set; }
     public int Data { get; set; }
@@ -15,9 +17,30 @@ public class Program
             new Item() { Id = 3, Data = 3 }
         };
 
+        DoSomeStuffWithDB();
+
         foreach (var item in items)
         {
             Console.WriteLine($"{item.Id}) - {item.Data}");
+        }
+    }
+
+    public static void DoSomeStuffWithDB()
+    {
+        using var conn = new SqliteConnection($"Data Source=items1.db; Foreign Keys=true; Mode=ReadOnly");
+        conn.Open();
+        string commandText = @"SELECT * FROM Items;";
+
+        var command = new SqliteCommand(commandText, conn);
+        using var reader = command.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var data = reader.GetInt32(1);
+                Console.WriteLine($"{id}) - {data}");
+            }
         }
     }
 }
